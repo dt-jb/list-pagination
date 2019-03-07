b37bfdf14029815312e124ee6a1220db7295b646
dt_jb <jbrown8013@gmail.com>
2019-03-07 11:34:29 +1100

    Complete- "Meets expectations" project

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -6,35 +6,17 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
+//Global constant declarations
 
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
 
 const studentList = document.getElementsByClassName('student-item');
 const numberOfPages = Math.floor(studentList.length / 10) + 1;
 const mainDiv = document.getElementsByClassName('page');
 
-
 /***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
+   Creates a function a function that takes a list and page number.  This
+   function displays a selection of students based on the page number and the
+   index of the students in the list, and hides the others.
 ***/
 
 const showPages = (list, page) => {
@@ -48,8 +30,9 @@ const showPages = (list, page) => {
 }
 
 /***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
+   Creates a function that adds the html for the page links based on the
+   number of students (and therefore, number of pages).  Also it adds
+   functionality to those page links (clickable).
 ***/
 const appendPageLinks = (list) => {
   const pageDiv = document.createElement('div');
@@ -76,11 +59,10 @@ const appendPageLinks = (list) => {
     }
     e.target.className = 'active';
   });
-  
 };
 
-//appendPageLinks();
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//Adding event listener to load only the first page upon initial page load.
+document.addEventListener('DOMContentLoaded', () => {
+  showPages(studentList, 1);
+});
+appendPageLinks();
